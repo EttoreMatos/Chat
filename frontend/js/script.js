fd6c564a06d6908636e2dc02107adf8fcc602f86
EttoreMatos <ettoremattos@gmail.com>
2025-08-03 16:08:49 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -32,7 +32,7 @@ const colors = [
 ];
 // Avatar padrão (círculo preto de 40x40 em PNG Base64)
 const defaultAvatar = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAArklEQVRYR+3WwQ2AIBRE0Sx+kOVZwVYKJ3hTYnSY9TxAIp9R2tHX9vMxNMGkD4C8AgkJgFZgg0YA2gDTAM7kAQA4DRhsMABGY2k3pcoAcNgD2FwB1Z/7x+9hyoDKB7pADaSvIVwlz4Gi6ARHtDT9tgCI4BMCm54A2VbbA9i8A32BpaV7rc3xEvkW2NY+j4c6Rr2nOtu3AqKzNSc0poD02pTbSCau+Kd7kH4Fxoc6+FquFNsAAAAASUVORK5CYII=";
-Notification.requestPermission()
+
 // Evento para mostrar preview do avatar ou padrão
 avatarInput.addEventListener("change", () => {
     const file = avatarInput.files[0];
@@ -82,19 +82,6 @@ const createMessageSelfElement = (content) => {
 };
 
 function createMessageOtherElement(content, sender, senderColor, avatarData) {
-    let preview;
-    if (/^<img[^>]*>/i.test(content.trim())) {
-      preview = "imagem";
-    } else {
-      // Remove HTML, limita a 50 caracteres
-      const temp = document.createElement("div");
-      temp.innerHTML = content;
-      preview = temp.textContent.trim().slice(0, 50);
-    }
-    new Notification("FastChat", {
-      body: `Mensagem de ${sender}: ${preview}`
-    });
-    
     audio_alert.play().catch((e) => {
         console.warn("Não foi possível tocar o som:", e.message);
     });
@@ -179,7 +166,6 @@ const processMessage = ({ data }) => {
     const parsed = JSON.parse(data);
 
     if (parsed.type === "connect") {
-      new Notification("FastChat", {body: `${user.name} entrou no FastChat`});
       audio_connect.play().catch((e) => {
         console.warn("Não foi possível tocar o som:", e.message);
       });
@@ -200,7 +186,6 @@ const processMessage = ({ data }) => {
     }
     
     if (parsed.type === "disconnect") {
-      new Notification("FastChat", {body: `${user.name} saiu do FastChat`});
       audio_disconnect.play().catch((e) => {
           console.warn("Não foi possível tocar o som:", e.message);
       });
@@ -291,7 +276,6 @@ function exit() {
 
 const handleLogin = async (event) => {
   event.preventDefault();
-
   const token = grecaptcha.getResponse();
   if (!token) {
       alert("Por favor, confirme que você não é um robô.");
